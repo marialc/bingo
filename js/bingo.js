9d8bfa2581060d600b1f56ebb8ded1235d45f240
María LC <marialc.spain@gmail.com>
2019-05-23 19:40:49 +0200

    Update bingo.js
    Ahora los números aleatorios ya no se repiten

--- js/bingo.js
+++ js/bingo.js
@@ -1,21 +1,9 @@
 function numbergenerator() {
-    var numbers = []; // Creo un array vacío
-
+    var numbers = [];
     for (var i=0;i<16;i++) {
-
         var numaleator = Math.floor(Math.random()*99) + 1;
-
-        /*
-        Math.floor redondea el numero resultante de Math.random
-        Math.random genera números aleatorios
-        En este caso 99 es hasta donde va a llegar contando y 1 es desde donde empieza a contar
-        */
-
-        numbers.push(numaleator);
-        // 'Push' mete los números aleatorios en el array 'numbers'
+        if (numbers.indexOf(numaleator) === -1) {numbers.push(numaleator);}
+        else {i--;}
         document.getElementById(+i).innerHTML = numbers[i];
-        // Coge los elementos correspondientes al id con el número de 'i' y mete dentro el número aleatorio que tenga el array en ese momento
-
     }
-
 }
