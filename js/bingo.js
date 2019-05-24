9c8278f09caf03b9f29be16bb60420cdef6a0313
María LC <marialc.spain@gmail.com>
2019-05-24 18:40:21 +0200

    Update bingo.js
    Definir colores aleatorios en las celdas coloreadas

--- js/bingo.js
+++ js/bingo.js
@@ -7,3 +7,38 @@ function numbergenerator() {
         document.getElementById(+i).innerHTML = numbers[i];
     }
 }
+
+// ----- DEFINIR UN COLOR ALEATORIO EN LAS CELDAS COLOREADAS -----
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i=0;i<6;i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor() {
+    $(".table-primary").css("background-color", getRandomColor());
+    }
+
+/* ----- DEFINIR UN COLOR ALEATORIO DE UNA SELECCIÓN DE COLORES -----
+
+// Esta es la mejor opción si quieres seleccionar tu mismx los colores de fondo para que combinen con los colores del resto de los elementos
+
+function colorRandomizer() {
+    var colorsArray = ['#5EFC8D','#F9C22E','#53B3CB','#88498F','#7DCFB6','#E15554','#7D5BA6','#E56399','#E9FF70','#FFD670','#61E786'];
+    var color;
+    
+    for(var i=0;i<12;i++) {
+       color = colorsArray[Math.floor(Math.random() * 12)];
+    }
+    return color;
+}
+
+function setRandomColor() {
+    $(".table-primary").css("background-color", colorRandomizer());
+    }
+    
+----------------------------------------------------------------------- */
